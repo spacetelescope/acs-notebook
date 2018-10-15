558d41903b81c6e35932fc056a154e493bc4be09
Melanie Kae Olaes <molaes@stsci.edu>
2018-10-15 12:39:52 -0400

    added p_module for convenience plotting in acs_pixel_area_maps

---
+++ acs_pixel_area_maps/p_module/plot.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.visualization import (ZScaleInterval, LinearStretch,
+                                   ImageNormalize)
+
+def ds9_imitate(ax,image):
+    norm = ImageNormalize(image,
+                          interval=ZScaleInterval(),
+                          stretch=LinearStretch())
+
+    ax.imshow(image, cmap='bone', norm=norm)
+    return
+
+def triple_pam_plot(flt_file, pam_file, figtitle):
+    fl_img = fits.getdata(flt_file, ext=1)
+    pam_img = fits.getdata(pam_file)
+
+    fig = plt.figure(figsize=(20,4))
+    fig.suptitle(figtitle,fontsize=20)
+
+    ax = fig.add_subplot(1, 3, 1)
+    ds9_imitate(ax, fl_img)
+    ax.set_title('Raw')
+
+    ax2 = fig.add_subplot(1, 3, 2, yticks=[])
+    ds9_imitate(ax2, pam_img)
+    ax2.set_title('Pixel Area Map')
+
+    pamd_img = fl_img*pam_img
+
+    ax3 = fig.add_subplot(1, 3, 3, yticks=[])
+    ds9_imitate(ax3, pamd_img)
+    ax3.set_title('Raw x Pixel Area Map')
+
+    plt.subplots_adjust(wspace=0.05)
+    return
